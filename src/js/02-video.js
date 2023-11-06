01a5b70d69d61cf3d7bfb6b848e4ffa77a1ea18d
sashasashkina <koshyl.apps@gmail.com>
2023-11-06 20:42:22 +0200

    seven

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,6 @@
 import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 const key = 'videoplayer-current-time';
 const playTime = localStorage.getItem(key);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,49 +1,63 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-const emailInput = document.querySelector('.feedback-form input');
-const textInput = document.querySelector('.feedback-form textarea');
+// const emailInput = document.querySelector('.feedback-form input');
+// const textInput = document.querySelector('.feedback-form textarea');
 
 const STORAGE_KEY = 'feedback-form-state';
 const formData = {};
 
-getDataFromStorage();
+// getDataFromStorage();
 
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
 function onFormInput(event) {
-  const formData = form.elements;
+  //   const formData = form.elements;
 
-  const email = formData.email.value;
-  const message = formData.message.value;
-  const dataToSave = { email, message };
-
+  //   const email = formData.email.value;
+  //   const message = formData.message.value;
+  //   const dataToSave = { email, message };
+  formData[event.target.name] = event.target.value.trim();
   localStorage.setItem(STORAGE_KEY, JSON.stringify(dataToSave));
 }
 
 function onFormSubmit(event) {
   event.preventDefault();
 
-  const {
-    elements: { email, message },
-  } = event.currentTarget;
+  //   const {
+  //     elements: { email, message },
+  //   } = event.currentTarget;
 
-  if (email.value === '' || message.value === '') {
-    return window.alert('Усі поля повинні бути заповнені!');
-  }
+  //   if (email.value === '' || message.value === '') {
+  //     return window.alert('Усі поля повинні бути заповнені!');
+  //   }
 
-  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
-
+  //   console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  console.log(formData);
+  formData = {};
   event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
 
-function getDataFromStorage() {
-  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+// function getDataFromStorage() {
+//   const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-  if (savedData) {
-    emailInput.value = savedData.email;
-    textInput.value = savedData.message;
+//   if (savedData) {
+//     emailInput.value = savedData.email;
+//     textInput.value = savedData.message;
+//   }
+// }
+refreshForm();
+function refreshForm() {
+  try {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    if (!savedData) return;
+    formData = JSON.parse(savedData);
+    Object.entries(formData).forEach(([key, val]) => {
+      form.elements[key].value = val;
+    });
+  } catch ({ message }) {
+    console.log(message);
   }
 }
