1fc7cb708997501321f83d9ede1008c07f249ed6
sashasashkina <koshyl.apps@gmail.com>
2023-11-07 11:21:50 +0200

    ten

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,25 +1,3 @@
-// import Player from '@vimeo/player';
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-
-// const key = 'videoplayer-current-time';
-// const playTime = localStorage.getItem(key);
-
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
-
-// function onPlay(event) {
-//   const time = event.seconds;
-//   localStorage.setItem(key, time);
-// }
-
-// if (playTime) {
-//   player.setCurrentTime(playTime);
-// }
-
-// player.on('timeupdate', throttle(onPlay, 1000));
-
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
